3564d1359117d64b14612d1da36615b7400399d7
Jia35 <31085601+Jia35@users.noreply.github.com>
2018-04-28 16:02:40 +0800

    Update hw12.py

--- file/code/hw12.py
+++ file/code/hw12.py
@@ -1,15 +1,8 @@
-x = []
 a = int(input('a:'))
 b = int(input('b:'))
 c = int(input('c:'))
 
-x.append(a)
-x.append(b)
-x.append(c)
-
-x.sort()
-
-if x[2] < x[0]+x[1]:
+if a+b>c and a+c>b and b+c>a:
     print('O')
 else:
     print('X')
